142dd668f36fdc0beea3788657fa166328eb34d1
Shubhangi <shubhangisrivastava@qainfotech.com>
2019-03-26 12:57:57 +0530

    final automation files

---
+++ QaInfotech/target/cucumber-reports/cucumber-pretty/report.js
@@ -0,0 +1,330 @@
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("Testscenarios.feature");
+formatter.feature({
+  "line": 2,
+  "name": "Open Qainfotech official website and explore",
+  "description": "",
+  "id": "open-qainfotech-official-website-and-explore",
+  "keyword": "Feature",
+  "tags": [
+    {
+      "line": 1,
+      "name": "@AutomationQa"
+    }
+  ]
+});
+formatter.before({
+  "duration": 11425216999,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 4,
+  "name": "Search bar is visible on the homepage qainfotech website",
+  "description": "",
+  "id": "open-qainfotech-official-website-and-explore;search-bar-is-visible-on-the-homepage-qainfotech-website",
+  "type": "scenario",
+  "keyword": "Scenario"
+});
+formatter.step({
+  "line": 5,
+  "name": "the user is on homepage of the qainfotech website",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 6,
+  "name": "he/she is able to see a search bar on top right corner of the page",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "HomePageStepDef.verifyQaInfotechHomepage()"
+});
+formatter.result({
+  "duration": 122989771,
+  "status": "passed"
+});
+formatter.match({
+  "location": "HomePageStepDef.verifysearchbar()"
+});
+formatter.result({
+  "duration": 54591126,
+  "status": "passed"
+});
+formatter.after({
+  "duration": 2458383,
+  "status": "passed"
+});
+formatter.before({
+  "duration": 2811367289,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 8,
+  "name": "When the user is in home page he/she is able to view images displaying in loop in top section",
+  "description": "",
+  "id": "open-qainfotech-official-website-and-explore;when-the-user-is-in-home-page-he/she-is-able-to-view-images-displaying-in-loop-in-top-section",
+  "type": "scenario",
+  "keyword": "Scenario"
+});
+formatter.step({
+  "line": 9,
+  "name": "the user is on homepage of the qainfotech website",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 10,
+  "name": "he/she is able to view images moving in loop  in image panel of the top section of the page",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "HomePageStepDef.verifyQaInfotechHomepage()"
+});
+formatter.result({
+  "duration": 23681549,
+  "status": "passed"
+});
+formatter.match({
+  "location": "HomePageStepDef.verifyimagepanel()"
+});
+formatter.result({
+  "duration": 216981949,
+  "status": "passed"
+});
+formatter.after({
+  "duration": 40635,
+  "status": "passed"
+});
+formatter.before({
+  "duration": 2206384691,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 12,
+  "name": "There is a pause button in the image panel and the image moving in loop are paused by it",
+  "description": "",
+  "id": "open-qainfotech-official-website-and-explore;there-is-a-pause-button-in-the-image-panel-and-the-image-moving-in-loop-are-paused-by-it",
+  "type": "scenario",
+  "keyword": "Scenario"
+});
+formatter.step({
+  "line": 13,
+  "name": "he/she is able to view a pause button on the image panel",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 14,
+  "name": "the user clicks on the pause button",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 15,
+  "name": "the image stops there and next image in loop do not come up",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "HomePageStepDef.verifypausebutton()"
+});
+formatter.result({
+  "duration": 232037288,
+  "status": "passed"
+});
+formatter.match({
+  "location": "HomePageStepDef.clickpause()"
+});
+formatter.result({
+  "duration": 81329269,
+  "status": "passed"
+});
+formatter.match({
+  "location": "HomePageStepDef.verifypausescreen()"
+});
+formatter.result({
+  "duration": 23080117,
+  "status": "passed"
+});
+formatter.after({
+  "duration": 53652,
+  "status": "passed"
+});
+formatter.before({
+  "duration": 2234322573,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 17,
+  "name": "There is a play button in the image panel and the image moving in loop are played by it",
+  "description": "",
+  "id": "open-qainfotech-official-website-and-explore;there-is-a-play-button-in-the-image-panel-and-the-image-moving-in-loop-are-played-by-it",
+  "type": "scenario",
+  "keyword": "Scenario"
+});
+formatter.step({
+  "line": 18,
+  "name": "he/she is able to view a play button on the image panel",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 19,
+  "name": "the user clicks on the play button",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 20,
+  "name": "the images starts moving again in loop",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "HomePageStepDef.verifyplaybutton()"
+});
+formatter.result({
+  "duration": 229663944,
+  "status": "passed"
+});
+formatter.match({
+  "location": "HomePageStepDef.clickplay()"
+});
+formatter.result({
+  "duration": 86511710,
+  "status": "passed"
+});
+formatter.match({
+  "location": "HomePageStepDef.verifyplayscreen()"
+});
+formatter.result({
+  "duration": 19921441,
+  "status": "passed"
+});
+formatter.after({
+  "duration": 274370,
+  "status": "passed"
+});
+formatter.before({
+  "duration": 2245314536,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 22,
+  "name": "The user is able to see a blue notepad icon on the home page of the website",
+  "description": "",
+  "id": "open-qainfotech-official-website-and-explore;the-user-is-able-to-see-a-blue-notepad-icon-on-the-home-page-of-the-website",
+  "type": "scenario",
+  "keyword": "Scenario"
+});
+formatter.step({
+  "line": 23,
+  "name": "the user is on homepage of the qainfotech website",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 24,
+  "name": "he/she is able to see a blue notepad icon in bottom right corner of the homepage",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "HomePageStepDef.verifyQaInfotechHomepage()"
+});
+formatter.result({
+  "duration": 31240336,
+  "status": "passed"
+});
+formatter.match({
+  "location": "HomePageStepDef.verifyblueicon()"
+});
+formatter.result({
+  "duration": 44345149,
+  "status": "passed"
+});
+formatter.after({
+  "duration": 68446,
+  "status": "passed"
+});
+formatter.before({
+  "duration": 2213362201,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 26,
+  "name": "The user is able to see a sales enquiry contacts number in the home page header",
+  "description": "",
+  "id": "open-qainfotech-official-website-and-explore;the-user-is-able-to-see-a-sales-enquiry-contacts-number-in-the-home-page-header",
+  "type": "scenario",
+  "keyword": "Scenario"
+});
+formatter.step({
+  "line": 27,
+  "name": "the user is on homepage of the qainfotech website",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 28,
+  "name": "the user is able to see \"Sales Enquiry:\" text and contact numbers along with it",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "HomePageStepDef.verifyQaInfotechHomepage()"
+});
+formatter.result({
+  "duration": 35427984,
+  "status": "passed"
+});
+formatter.match({
+  "location": "HomePageStepDef.verifycontactdetails()"
+});
+formatter.result({
+  "duration": 67457021,
+  "status": "passed"
+});
+formatter.after({
+  "duration": 48086,
+  "status": "passed"
+});
+formatter.before({
+  "duration": 2240032359,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 30,
+  "name": "Verify user clicks on \"Careers\" link available on topright corner of website homepage then he navigates to another page about career information",
+  "description": "",
+  "id": "open-qainfotech-official-website-and-explore;verify-user-clicks-on-\"careers\"-link-available-on-topright-corner-of-website-homepage-then-he-navigates-to-another-page-about-career-information",
+  "type": "scenario",
+  "keyword": "Scenario"
+});
+formatter.step({
+  "line": 31,
+  "name": "the user is on homepage of the qainfotech website",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 32,
+  "name": "he/she clicks the \"Careers\" link on topright corner",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 33,
+  "name": "he/she is navigated to another page dedicated for giving information about careers",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "HomePageStepDef.verifyQaInfotechHomepage()"
+});
+formatter.result({
+  "duration": 28105597,
+  "status": "passed"
+});
+formatter.match({
+  "location": "HomePageStepDef.clickCareerlink()"
+});
+formatter.result({
+  "duration": 4084551050,
+  "status": "passed"
+});
+formatter.match({
+  "location": "HomePageStepDef.verifycareerpagenavigation()"
+});
+formatter.result({
+  "duration": 33025165,
+  "status": "passed"
+});
+formatter.after({
+  "duration": 49803,
+  "status": "passed"
+});
